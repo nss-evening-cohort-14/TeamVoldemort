4d1955228d8f3539668dbdecf79ddd7c700f8f92
BenJarrett <b.cl.jarrett@gmail.com>
2021-08-10 18:13:16 -0500

    Basic Repositories

--- TeamVoldemort/Repositories/DistrictRepository.cs
+++ TeamVoldemort/Repositories/DistrictRepository.cs
@@ -8,43 +8,43 @@ namespace TeamVoldemort.Repositories
 {
     class DistrictRepository
     {
-        //class DistrictRepository
-        //{
-        //    // we're using a static list here instead of a proper database
-        //    // but the general idea still holds up once we cover databases
-        //    // it's just another piece of code to replace and refactor nbd
-        //    static List<District> _districts = new List<District>();
+        class DistrictRepository
+        {
+            // we're using a static list here instead of a proper database
+            // but the general idea still holds up once we cover databases
+            // it's just another piece of code to replace and refactor nbd
+            static List<District> _districts = new List<District>();
 
-        //    public List<District> GetDistricts()
-        //    {
-        //        throw new NotImplementedException();
-        //    }
+            public List<District> GetDistricts()
+            {
+                throw new NotImplementedException();
+            }
 
-        //    public void SaveNewDistrict(District district)
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-        //}
-        
-        //class StoreRepository
-        //{
-        //    static List<Store> _stores = new List<Store>();
-        //    public List<Store> GetAllStores();
-        //}
+            public void SaveNewDistrict(District district)
+            {
+                throw new NotImplementedException();
+            }
+        }
 
-        //class SalesRepository
-        //{
-        //    static List<Sales> _sales = new List<Sales>();
-        //    public List<Sales> GetAllSales();
-        //}
+        class StoreRepository
+        {
+            static List<Store> _stores = new List<Store>();
+            public List<Store> GetAllStores();
+        }
 
-        //class PeopleRepository
-        //{
-        //    static List<Person> _people = new List<Person>
-        //    {
-        //        // Field for Employee Info //
-        //    };
-        //}
+        class SalesRepository
+        {
+            static List<Sales> _sales = new List<Sales>();
+            public List<Sales> GetAllSales();
+        }
+
+        class PeopleRepository
+        {
+            static List<Person> _people = new List<Person>
+            {
+                // Field for Employee Info //
+            };
+        }
 
 
     }
