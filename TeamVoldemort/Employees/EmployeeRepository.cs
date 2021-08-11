e8958a8dd04b85fb24dcf03e99ad2c166677e160
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-08-10 19:10:22 -0500

    Fixed file structure

---
+++ TeamVoldemort/Employees/EmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TeamVoldemort.Employees
+{
+    class EmployeeRepository
+    {
+        static List<Employee> _employees = new List<Employee>
+        {
+            // Field for Employee Info //
+        };
+    }
+}
+
