46b7265ff9b010ba86e4ce3ba4e33eb0c683d94d
Caseywalker <casey.walker118@gmail.com>
2021-08-12 18:44:29 -0500

    Adding store enums.

--- TeamVoldemort/Districts/DistrictRepository.cs
+++ TeamVoldemort/Districts/DistrictRepository.cs
@@ -12,7 +12,7 @@ namespace TeamVoldemort.Districts
     // but the general idea still holds up once we cover databases
     // it's just another piece of code to replace and refactor nbd
     static List<District> _districts = new List<District> {
-             new District("Bob", "Big South")
+             new District("Bob", DistrictName.South)
             };
             
 
