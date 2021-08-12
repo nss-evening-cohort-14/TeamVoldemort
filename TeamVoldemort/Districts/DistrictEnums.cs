87987d3ebef9f3f7d414d5599907e2f7c05d78a9
Caseywalker <casey.walker118@gmail.com>
2021-08-12 17:54:46 -0500

    Adding district repositories

---
+++ TeamVoldemort/Districts/DistrictEnums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamVoldemort.Districts
+{
+  enum DistrictName
+  {
+   South,
+   North,
+   Central,
+   West
+  }
+}
--- TeamVoldemort/Program.cs
+++ TeamVoldemort/Program.cs
@@ -11,28 +11,7 @@ namespace TeamVoldemort
             Console.WriteLine("Hello World!");
             var menu = new DisplayMenu();
             var userInput =  menu.Display();
-      switch (userInput) 
-      {
-        case 1:
-          Console.WriteLine("You selected Enter District Sales");
-          break;
-        case 2:
-          StoreRepository.GenerateDistrictReport();
-          menu.Display();
-          break;
-        case 3:
-          Console.WriteLine("You selected Add New Employee");
-          break;
-        case 4:
-          Console.WriteLine("You selected Add a Store/District");
-          break;
-        case 5:
-          Console.WriteLine("You selected Exit");
-          break;
-        default:
-          Console.WriteLine("Invalid Entry");
-          break;
-      }
+            menu.UserMenu(userInput);
 
     }
   }
