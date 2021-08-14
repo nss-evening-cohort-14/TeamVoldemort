7b2ef8426f74d9ff31319cba3a192e67e87e3c25
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-08-14 10:43:53 -0500

    Added ID

--- TeamVoldemort/Employees/EmployeeRepository.cs
+++ TeamVoldemort/Employees/EmployeeRepository.cs
@@ -7,21 +7,18 @@ namespace TeamVoldemort.Employees
     {
     static List<Employee> _employees = new List<Employee>
     {
-    new Employee("Jim", "AssistantManager", 4312, 1),
-    new Employee("Bob", "Store Manager", 5612, 1),
-    new Employee("Sarah", "Associate", 4586, 1),
-    new Employee("Roger", "AssistantManager", 3207, 2),
-    new Employee("Shirley", "Store Manager", 7328, 2),
-    new Employee("Sarah", "Associate", 7328, 2),
-    new Employee("Samantha", "AssistantManager", 3456, 3),
-    new Employee("Rachel", "Store Manager", 5612, 3),
-    new Employee("Kyle", "Associate", 3456, 3),
-    new Employee("Cheyenne", "AssistantManager", 9375, 4),
-    new Employee("Lilly", "Store Manager", 5612, 4),
-    new Employee("Nick", "Associate", 3456, 4),
-
-
-
+    new Employee("Jim", 1, "AssistantManager", 4312, 1),
+    new Employee("Bob", 2, "Store Manager", 5612, 1),
+    new Employee("Sarah", 3, "Associate", 4586, 1),
+    new Employee("Roger", 4, "AssistantManager", 3207, 2),
+    new Employee("Shirley", 5, "Store Manager", 7328, 2),
+    new Employee("Sarah", 6, "Associate", 7328, 2),
+    new Employee("Samantha", 7, "AssistantManager", 3456, 3),
+    new Employee("Rachel", 8, "Store Manager", 5612, 3),
+    new Employee("Kyle", 9, "Associate", 3456, 3),
+    new Employee("Cheyenne", 10, "AssistantManager", 9375, 4),
+    new Employee("Lilly", 11, "Store Manager", 5612, 4),
+    new Employee("Nick", 12, "Associate", 3456, 4),
 
     };
     public static void GenerateEmployeeReport(int storeNumber)
@@ -45,13 +42,18 @@ namespace TeamVoldemort.Employees
         }
       }
     }
+
+
         public static void AddNewEmployee()
         {
             Console.Clear();
-            var userEmployee = new Employee("Colin", "Associate", 4857, 1);
+            var userEmployee = new Employee("Colin", 13, "Associate", 4857, 1);
 
             Console.WriteLine("Enter Employee Name");
             userEmployee.EmployeeName = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee ID Number:");
+            userEmployee.EmployeeID = Int32.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter Employee Title");
             userEmployee.EmployeeTitle = Console.ReadLine();
@@ -70,15 +72,28 @@ namespace TeamVoldemort.Employees
             Console.WriteLine("                 ");
             Console.WriteLine($"\tName: {userEmployee.EmployeeName}");
             Console.WriteLine("                 ");
+            Console.WriteLine($"\tID Number: {userEmployee.EmployeeID}");
+            Console.WriteLine("                 ");
             Console.WriteLine($"\tStore: #{userEmployee.StoreNumber}");
             Console.WriteLine("                 ");
             Console.WriteLine($"\tTitle: {userEmployee.EmployeeTitle}");
             Console.WriteLine("                 ");
             Console.WriteLine($"\tEmployee Sales: ${userEmployee.EmployeeSales}");
             Console.WriteLine("                 ");
-
-
         }
+
+        //public static void updateEmployeeInfo()
+        //{
+        //    Console.WriteLine("Update employee information? Y/N");
+        //    var response = Console.ReadLine().ToLower();
+        //    var yes = 'y';
+        //    var no = 'n';
+
+        //    if (String.Equals(response, yes))
+        //    {
+
+        //    }
+        //}
     }
 }
 
